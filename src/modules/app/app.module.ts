1a48ed27650111f3c2102c2ef2dd96b81b7d6b7d
nenertiy <nenertiy@gmail.com>
2025-03-17 09:29:07 +0300

    update app module and main.ts

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 import { S3Module } from 'nestjs-s3';
+import { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-ioredis';
 import config from 'src/config/config';
+import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
+import { TokenModule } from '../token/token.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => {
+        return {
+          store: redisStore,
+          host: configService.get('REDIS_URL'),
+          port: 6379,
+          ttl: 60 * 60,
+          max: 1000000000000000000,
+        };
+      },
+    }),
     S3Module.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -23,6 +43,9 @@ import config from 'src/config/config';
         },
       }),
     }),
+    UsersModule,
+    AuthModule,
+    TokenModule,
   ],
 })
 export class AppModule {}
